d47a02e77a400cab0bc65db7861bfacd791880c8
baronRobotXXI <79488584+baronRobotXXI@users.noreply.github.com>
2022-07-25 20:21:20 -0300

    Add files via upload

--- lab10_2/fila_cont.h
+++ lab10_2/fila_cont.h
@@ -3,6 +3,11 @@
 
 typedef struct fila Fila;
 
-
+Fila* CriaFila();
+int FilaVazia (Fila* f);
+int FilaCheia (Fila* f);
+int InsereFim (Fila* f, int elem);
+int RemoveInicio (Fila* f, int* elem);
+void ImprimeFila (Fila* f);
 
 #endif // FILA_CONT_H_INCLUDED
